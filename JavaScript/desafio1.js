bd31805feb2209bf0a5f5100cdb5eb0013f1353a
talu <talita.santos96@hotmail.com>
2022-01-16 22:17:52 -0300

    12.01.2022 | Desafio 1 com JS

---
+++ JavaScript/desafio1.js
@@ -0,0 +1,21 @@
+//Bloco 2 - 12.01.2022
+
+//Crie uma variável que receba um nome
+// Crie uma variável que receba um número;
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+//Mostre no console a mensagem "Estou mexendo no console.log"
+
+
+let nome = "Talita Santos";
+let numero = 100;
+let anoAtual = 2022;
+let subtracao = anoAtual - numero;
+let soma = numero + anoAtual;
+
+console.log('Variável com nome:', nome);
+console.log('Variável com número:', numero);
+console.log('Ano atual:', anoAtual);
+console.log('Resultado da subtração:', subtracao);
+console.log('Resultado da soma:', soma)
+console.log("Estou mexendo no console.log");
